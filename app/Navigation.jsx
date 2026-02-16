5cce9e6b0a8812200a29617a11d346c6bfde3b9d
Tejas <tejasspatell2@gmail.com>
2026-02-16 12:50:43 +0530

    learning next

---
+++ app/Navigation.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function Navigation() {
+  return (
+    <header>
+        <div>
+
+        </div>
+        <nav>
+            <ul>
+                <li>
+                    <a href='/home'>Home</a>
+                </li>
+                <li>Setting</li>
+                <li>
+                    <a href='/about'>About</a>
+                </li>
+                <li></li>
+                <li></li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
