382d3e984fb57fdc0b36d4e8d250b231b172b030
Tejas <tejasspatell2@gmail.com>
2026-02-18 23:13:52 +0530

    learning next

---
+++ app/(users)/services/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const metadata = {
+    title: "Service Page",
+    description: "This is service page",
+    keywords: ["nextjs", "reactjs"]
+} 
+
+export default function page() {
+  return (
+    <div>
+        this is service page
+    </div>
+  )
+}
