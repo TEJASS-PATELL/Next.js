579dab495c033e00ed2e27df9a69a80363b4a55f
Tejas <tejasspatell2@gmail.com>
2026-02-18 13:20:55 +0530

    learning next

--- app/(users)/layout.js
+++ app/(users)/layout.js
@@ -1,9 +1,15 @@
 import Navigation from "../../components/Navigation";
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+})
 
 export default function RootLayout({ children }) {
   return (
     <html>
-      <body>
+      <body className={roboto.className}>
         <Navigation /> 
         {children}
       </body>
